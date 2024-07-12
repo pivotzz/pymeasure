85b2b32fea07c6873f07230de2006dbdf3dbbc0c
pivotzz <139361133+pivotzz@users.noreply.github.com>
2024-07-12 18:58:46 +0800

    trying to connect or-x325
    pyvisa and serial packages has some issues. tried this on macbook, possibly the reason

---
+++ find_visa.py
@@ -0,0 +1,10 @@
+import pyvisa
+
+def list_resources():
+    rm = pyvisa.ResourceManager()
+    resources = rm.list_resources()
+    for resource in resources:
+        print(resource)
+
+if __name__ == "__main__":
+    list_resources()
